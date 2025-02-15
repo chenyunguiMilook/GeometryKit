c69a9775774ba80b06681fbd830c9a42e4260deb
chenyungui <linxitj2003@gmail.com>
2025-02-15 17:33:59 +0800

    add Matrix2D

---
+++ Sources/GeometryKit/Matrix2D.swift
@@ -0,0 +1,13 @@
+//
+//  Matrix2D.swift
+//  GeometryKit
+//
+//  Created by yungui chen on 2025/2/15.
+//
+
+import Foundation
+import CoreGraphics
+
+public typealias Matrix2D = CGAffineTransform
+
+
